50e07153e5c0934e60b1c01451eb911ac6d80703
Guillermo de la Torre <jawilliam@gmail.com>
2019-05-05 18:06:01 -0300

    readonly annotations (small fixes)

--- Jawilliam.CDF.Tests/CSharp/RDSLTests.cs
+++ Jawilliam.CDF.Tests/CSharp/RDSLTests.cs
@@ -13,7 +13,7 @@ namespace Jawilliam.CDF.Tests.CSharp
     public class RDSLTests
     {
         [TestMethod]
-        public void GuidelinesForTopologicalProperties()
+        public void GuidelinesForTopologicallyRelevantProperties()
         {
             var rdsl = CDF.XObjects.RDSL.Syntax.Load(@"..\..\..\Jawilliam.CDF.CSharp\RDSL.xml");
             var concreteTypes = rdsl.Nodes.Type.Where(n => !n.@abstract).ToArray();
@@ -77,6 +77,81 @@ namespace Jawilliam.CDF.Tests.CSharp
                 expressionProperties.Union(operatorProperties).Union(identifierTokenProperties).Union(otherProperties)
             ).ToArray();
             Assert.AreEqual(topologicalButNonExpressionProperties.Length, 0);
+        }
+
+        [TestMethod]
+        public void GuidelinesForReadOnlyTypes()
+        {
+            var rdsl = CDF.XObjects.RDSL.Syntax.Load(@"..\..\..\Jawilliam.CDF.CSharp\RDSL.xml");
+            var concreteTypes = rdsl.Nodes.Type.Where(n => !n.@abstract).ToArray();
+
+            var readonlyTypes = (from t in concreteTypes
+                                 where t.@readonly
+                                 select t).ToArray();
+
+            var typesWithJustReadOnlyProperties = (from t in concreteTypes
+                                                   where t.Properties?.Property.All(p => p.readOnly) ?? false
+                                                   select t).ToArray();
+
+            var readonlyTypesWithJustReadOnlyProperties = typesWithJustReadOnlyProperties.Except(readonlyTypes).ToArray();
+            var readonlyTypesWithJustReadOnlyProperties2 = readonlyTypes.Except(typesWithJustReadOnlyProperties).ToArray();
+            //var expressionButNonTopologicalProperties = expressionProperties.Except(readonlyTypes).ToArray();
+            ;
+
+            //var expressionProperties = (from t in concreteTypes
+            //                            from p in t.Properties?.Property
+            //                            where p.kind == "Expression"
+            //                            select new { Type = t, Property = p }).ToArray();
+
+            //// All expression property is topologically relevant.
+            //var expressionButNonTopologicalProperties = expressionProperties.Except(readonlyTypes).ToArray();
+            //Assert.AreEqual(expressionButNonTopologicalProperties.Length, 0);
+
+            //var operatorProperties = (from t in concreteTypes
+            //                          from p in t.Properties?.Property
+            //                          where p.@operator ?? false
+            //                          select new { Type = t, Property = p }).ToArray();
+
+            //// All operator property is topologically relevant.
+            //var operatorButNonTopologicalProperties = operatorProperties.Except(readonlyTypes).ToArray();
+            //Assert.AreEqual(operatorButNonTopologicalProperties.Length, 0);
+
+            //var identifierTokenProperties = (from t in concreteTypes
+            //                                 from p in t.Properties?.Property
+            //                                 where p.kind == "Token" && (p.Rules?.Name?.Equality.full ?? false) &&
+            //                                       !(p.name == "ThisKeyword" && t.name == "IndexerDeclarationSyntax") &&
+            //                                       !(p.name == "ThisKeyword" && t.name == "IndexerMemberCrefSyntax")
+            //                                 select new { Type = t, Property = p }).ToArray();
+
+            //// All identifier token property is topologically relevant.
+            //var identifierTokenButNonTopologicalProperties = identifierTokenProperties.Except(readonlyTypes).ToArray();
+            //Assert.AreEqual(identifierTokenButNonTopologicalProperties.Length, 0);
+
+            //// Exceptions
+            //var otherProperties = (from t in concreteTypes
+            //                       from p in t.Properties?.Property
+            //                       where (p.name == "Token" && t.name == "LiteralExpressionSyntax") ||
+            //                             (p.name == "TextToken" && t.name == "InterpolatedStringTextSyntax") ||
+            //                             (p.name == "FormatStringToken" && t.name == "InterpolationFormatClauseSyntax") ||
+            //                             (p.name == "Keyword" && t.name == "PredefinedTypeSyntax") ||
+            //                             (p.name == "CaseOrDefaultKeyword" && t.name == "GotoStatementSyntax") ||
+            //                             (p.name == "VarianceKeyword" && t.name == "TypeParameterSyntax") ||
+            //                             (p.name == "Line" && t.name == "LineDirectiveTriviaSyntax") ||
+            //                             (p.name == "File" && t.name == "LineDirectiveTriviaSyntax") ||
+            //                             (p.name == "DisableOrRestoreKeyword" && t.name == "PragmaWarningDirectiveTriviaSyntax") ||
+            //                             (p.name == "File" && t.name == "PragmaChecksumDirectiveTriviaSyntax") ||
+            //                             (p.name == "Guid" && t.name == "PragmaChecksumDirectiveTriviaSyntax") ||
+            //                             (p.name == "Bytes" && t.name == "PragmaChecksumDirectiveTriviaSyntax") ||
+            //                             (p.name == "File" && t.name == "ReferenceDirectiveTriviaSyntax") ||
+            //                             (p.name == "File" && t.name == "LoadDirectiveTriviaSyntax")
+            //                       select new { Type = t, Property = p }).ToArray();
+            //var otherPropertiesButNonTopologicalProperties = otherProperties.Except(readonlyTypes).ToArray();
+            //Assert.AreEqual(otherPropertiesButNonTopologicalProperties.Length, 0);
+
+            //var topologicalButNonExpressionProperties = readonlyTypes.Except(
+            //    expressionProperties.Union(operatorProperties).Union(identifierTokenProperties).Union(otherProperties)
+            //).ToArray();
+            //Assert.AreEqual(topologicalButNonExpressionProperties.Length, 0);
         }
     }
 }
