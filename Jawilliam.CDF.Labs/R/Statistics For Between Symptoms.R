18bdc08f0f0bea37722a2e50e6ad2dbd1666fe11
Guillermo de la Torre <jawilliam@gmail.com>
2019-10-18 19:40:22 -0300

    progress

--- Jawilliam.CDF.Labs/R/Statistics For Between Symptoms.R
+++ Jawilliam.CDF.Labs/R/Statistics For Between Symptoms.R
@@ -2,9 +2,21 @@ library(readr)
 
 BetweenSymptomsStats <- read_delim("D:/ExperimentLogs/BetweenSymptomsStats_28Vs30 - copia.txt", ";", escape_double = FALSE, trim_ws = TRUE)
 
-projects <- unique(BetweenSymptomsStats$Project)
-projects <- data.frame(projects)
-names(projects)[1] <- colnames(BetweenSymptomsStats)[1]
+#projects <- unique(BetweenSymptomsStats$Project)
+#projects <- data.frame(projects)
+#names(projects)[1] <- colnames(BetweenSymptomsStats)[1]
+
+#approaches <- data.frame(approach=factor(), a28_Bp=double(), m28_Bp=double(), 
+#                                               a29_BW=double(), m29_BW=double(), 
+#                                               a30_BpIt=double(), m30_BpIt=double(),
+#                                               a31_BW=double(), m31_BW=double(),
+#                                               a32_It=double(), m32_It=double(),
+#                                               a33_BW=double(), m33_BW=double(),
+#                                               a34_BpDe=double(), m34_BpDe=double(),
+#                                               a35_BW=double(), m35_BW=double(),
+#                                               a36_BpDeIt=double(), m36_BpDeIt=double(),
+#                                               a37_BW=double(), m37_BW=double() )
+View(approaches)
 
 #stats <- data.frame(BetweenSymptomsStats2_copia$Project, BetweenSymptomsStats2_copia$PrincipalRevisionPair)
 #stats30Vs31 <- BetweenSymptomsStats2_copia[(BetweenSymptomsStats2_copia$`#lr_matches_31` != BetweenSymptomsStats2_copia$`#rl_matches_31`) | 
@@ -14,21 +26,18 @@ names(projects)[1] <- colnames(BetweenSymptomsStats)[1]
 #                                           (BetweenSymptomsStats2_copia$`#lr_updates_31` != BetweenSymptomsStats2_copia$`#rl_updates_31`) |
 #                                           (BetweenSymptomsStats2_copia$`#lr_moves_31` != BetweenSymptomsStats2_copia$`#rl_moves_31`),]
 
-allmismatches <- BetweenSymptomsStats[which(BetweenSymptomsStats[27] != 0),]
-View(allmismatches)
-allmisactions <- BetweenSymptomsStats[which(BetweenSymptomsStats[16] != BetweenSymptomsStats[22]),]
-View(allmisactions)
+BetweenSymptomsStats <- read_delim("D:/ExperimentLogs/BetweenSymptomsStats_28Vs30.txt", ";", escape_double = FALSE, trim_ws = TRUE)
 
-lBest <- BetweenSymptomsStats[which(BetweenSymptomsStats[16] < BetweenSymptomsStats[22]),]
-print("Left Best: ")
-nrow(lBest)
-rBest <- BetweenSymptomsStats[which(BetweenSymptomsStats[16] > BetweenSymptomsStats[22]),]
-print("Right Best")
-nrow(rBest)
-noBest <- BetweenSymptomsStats[which(BetweenSymptomsStats[16] == BetweenSymptomsStats[22]),]
-print("No Best")
-nrow(noBest)
+stats <- data.frame(Left_Best_Actions=nrow(BetweenSymptomsStats[which(BetweenSymptomsStats[16] < BetweenSymptomsStats[22]),]), 
+                    Right_Best_Actions=nrow(BetweenSymptomsStats[which(BetweenSymptomsStats[16] > BetweenSymptomsStats[22]),]), 
+                    Equal_Actions=nrow(BetweenSymptomsStats[which(BetweenSymptomsStats[16] == BetweenSymptomsStats[22]),]),
+                    NotEqual_Actions=nrow(BetweenSymptomsStats[which(BetweenSymptomsStats[16] != BetweenSymptomsStats[22]),]),
+                    ALL_Mismatches=nrow(BetweenSymptomsStats[which(BetweenSymptomsStats[27] != 0),]), 
+                    LR_Mismatches=nrow(BetweenSymptomsStats[which(BetweenSymptomsStats[28] != 0),]), 
+                    RL_Mismatches=nrow(BetweenSymptomsStats[which(BetweenSymptomsStats[29] != 0),]))
+View(stats)
 
+View(BetweenSymptomsStats[which(BetweenSymptomsStats[16] != BetweenSymptomsStats[22]),])
 #for (i in 1:nrow(stats)) 
 #{
 #  stats[i,1]
